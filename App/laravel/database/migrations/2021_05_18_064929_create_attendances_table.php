dce3e22be7afb778a08fda234b66481d0f55fe28
EhsanPopal <popalehsan26@gmail.com>
2021-05-18 11:42:02 +0430

    attendace, chance, finalMarks, mark, midtermmarks, semester, studnet, subject, teacher models created

---
+++ App/laravel/database/migrations/2021_05_18_064929_create_attendances_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->enum('state', ['PRESENT', 'ABSENT']);
+            $table->date('date');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->foreignId('student_id')->constrained('students');
+            $table->foreignId('subject_id')->constrained('subjects');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
