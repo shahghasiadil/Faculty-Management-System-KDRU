5a4b3d7cea523db35f648c81fb26c199e12b8751
EhsanPopal <popalehsan26@gmail.com>
2021-05-20 10:59:49 +0430

    student-subject table added, error cleared, admin table removed

--- App/laravel/database/migrations/2021_05_20_062308_student_subject.php
+++ App/laravel/database/migrations/2021_05_20_062308_student_subject.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTableStudentSubject extends Migration
+class StudentSubject extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateTableStudentSubject extends Migration
      */
     public function up()
     {
-        Schema::create('student_subject', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
             $table->foreignId('student_id')->constrained();
             $table->foreignId('subject_id')->constrained();
             $table->date('date');
-            $table->id();
-            $table->timestamps();
         });
     }
 
@@ -29,6 +29,6 @@ class CreateTableStudentSubject extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('student_subject');
+        //
     }
 }
