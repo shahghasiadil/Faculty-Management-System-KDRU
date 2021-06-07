60c1629630e85de92650ed65e6964a704233ec8a
EhsanPopal <popalehsan26@gmail.com>
2021-06-07 18:36:14 +0430

    soft delete utility added to schedule model and it works

--- App/laravel/app/Models/Schedule.php
+++ App/laravel/app/Models/Schedule.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Schedule extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $fillable = ['week_day', 'teacher_id', 'subject_id', 'start_time', 'end_time'];
     public function teacher()
     {
