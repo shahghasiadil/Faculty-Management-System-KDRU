a9f595427ddcb80a5f9b3ecef987846b85d105d4
ibrahimkhalil.cs30 <ibrahimkhalil.cs30@gmail.com>
2021-06-07 12:36:21 +0430

    the semester crud has been created

--- App/laravel/routes/api.php
+++ App/laravel/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\SubjectController;
+use App\Http\Controllers\SemesterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('/subjects', SubjectController::class);
+Route::apiResource('/semester', SemesterController::class);
