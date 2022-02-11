45b5fa41404f64e3c3ac6247f0dfda09fb108b06
shahghasiadil <shaghasy555@gmail.com>
2022-02-11 20:21:30 +0430

    sidebar refactored

--- App/client/src/navigation/vertical/index.js
+++ App/client/src/navigation/vertical/index.js
@@ -1,12 +1,7 @@
 // ** Navigation sections imports
 import apps from './apps'
-import pages from './pages'
-import forms from './forms'
-import tables from './tables'
-import others from './others'
+
 import dashboards from './dashboards'
-import uiElements from './ui-elements'
-import chartsAndMaps from './charts-maps'
 
 // ** Merge & Export
-export default [...dashboards, ...apps, ...pages, ...uiElements, ...forms, ...tables, ...chartsAndMaps, ...others]
+export default [...dashboards, ...apps]
