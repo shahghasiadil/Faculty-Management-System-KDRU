20374b7ba0bdfc4a5343307939e8b2a4e48fb7a0
zabihullah hamdard <zabih036@gmail.com>
2022-02-24 12:06:02 +0430

    Student Attendance Report Done

--- App/client/src/views/attendances/list/index.js
+++ App/client/src/views/attendances/list/index.js
@@ -1,93 +1,137 @@
-// ** Styles
-import '@styles/react/apps/app-users.scss'
-// ** React Imports
-import { Fragment, useState, useEffect } from 'react'
+import { Fragment } from 'react'
+import { Link } from 'react-router-dom'
+import Avatar from '@components/avatar'
+import { CardImg, Row, Col, Table, Breadcrumb, BreadcrumbItem } from 'reactstrap'
 
+import img1 from '@src/assets/images/slider/06.jpg'
+import img10 from '@src/assets/images/avatars/10.png'
 
-// ** Third Party Components
-import Select from 'react-select'
+const CardActions = () => {
+  const selectedStudent = {}
 
-import { selectThemeColors } from '@utils'
-import { Card, CardHeader, CardTitle, CardBody, Input, Row, Col, Label, CustomInput, Button } from 'reactstrap'
-
-// ** Styles
-import '@styles/react/libs/react-select/_react-select.scss'
-
-const ChancesList = () => {
-
-  const [currentRole, setCurrentRole] = useState({ value: '', label: 'Select Role' })
-  const [currentPlan, setCurrentPlan] = useState({ value: '', label: 'Select Plan' })
-  const [currentStatus, setCurrentStatus] = useState({ value: '', label: 'Select Status', number: 0 })
-      // ** User filter options
-  const roleOptions = [
-    { value: '', label: 'Select Role' },
-    { value: 'admin', label: 'Admin' },
-    { value: 'author', label: 'Author' },
-    { value: 'editor', label: 'Editor' },
-    { value: 'maintainer', label: 'Maintainer' },
-    { value: 'subscriber', label: 'Subscriber' }
-  ]
-
-  const planOptions = [
-    { value: '', label: 'Select Plan' },
-    { value: 'basic', label: 'Basic' },
-    { value: 'company', label: 'Company' },
-    { value: 'enterprise', label: 'Enterprise' },
-    { value: 'team', label: 'Team' }
-  ]
-
-  const statusOptions = [
-    { value: '', label: 'Select Status', number: 0 },
-    { value: 'pending', label: 'Pending', number: 1 },
-    { value: 'active', label: 'Active', number: 2 },
-    { value: 'inactive', label: 'Inactive', number: 3 }
-  ]
   return (
-    <div className='app-user-list'>
-            <Card>
-        <CardHeader>
-          <CardTitle tag='h4'>Search Filter</CardTitle>
-        </CardHeader>
-        <CardBody>
-          <Row>
-            <Col md='4'>
-              <Select
-                isClearable={false}
-                theme={selectThemeColors}
-                className='react-select'
-                classNamePrefix='select'
-                options={roleOptions}
-                value={currentRole}
-              />
-            </Col>
-            <Col className='my-md-0 my-1' md='4'>
-              <Select
-                theme={selectThemeColors}
-                isClearable={false}
-                className='react-select'
-                classNamePrefix='select'
-                options={planOptions}
-                value={currentPlan}
+    <Fragment>
+      <Breadcrumb>
+        <BreadcrumbItem>
+          <Link to='/students'> Students </Link>
+        </BreadcrumbItem>
+        <BreadcrumbItem active>
+          <span> Attendance </span>
+        </BreadcrumbItem>
+      </Breadcrumb>
 
-              />
-            </Col>
-            <Col md='4'>
-              <Select
-                theme={selectThemeColors}
-                isClearable={false}
-                className='react-select'
-                classNamePrefix='select'
-                options={statusOptions}
-                value={currentStatus}
+      <div className='attendance-div'>
+        <Row className='attendance-top-row' md='12' >
+          <Col md='4' className='attendance-logo-1'>
+            <Avatar className='mr-1 ' img={img1} imgHeight='90' imgWidth='90' />
+          </Col>
+          <Col md='4' className='top-center-div'>
+            <Row></Row>
+            <Row><h4>کندهار پوهنتون</h4></Row>
+            <Row><h4>کمپیوټر ساینس پوهنځی</h4></Row>
+            <Row><h4>د تدریسې چارو عمومې مدیریت</h4></Row>
+          </Col>
+          <Col md='4'>
+            <Avatar className='mr-1 ' img={img1} imgHeight='90' imgWidth='90' />
+          </Col>
+        </Row>
+        <Col md='12' className='text-center'>
+          <Table bordered className='table-student-info table-bordered'>
+            <tr style={{ borderTop: 0 }}>
+              <td colSpan="25" >
+                <h4>د ۱۴۰۰-۱۳۹۹ ل تحصیلې کال د لومړی ټولګې لومړې سمسټر د (&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;) مضمون د  (&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;) استاد (&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;) کریډیټه قوس د میاشتې حاضرې</h4>
+              </td>
+            </tr>
+            <tr style={{ textAlign: 'center' }}>
+              <th rowSpan="3">کتنی</th>
+              <th colSpan="2">حاضری</th>
+              <th colSpan="16" className="test">3</th>
+              <th colSpan="4">شهرت</th>
+              <th rowSpan="3" className='col-code-number' style={{ padding: '0' }}>کود نمبر</th>
+              <th rowSpan="3" className='col-gana' style={{ width: '10px' }}>گنه</th>
+            </tr>
+            <tr>
+              <th rowSpan="2" className='col-absent' style={{ padding: '0' }}> غیرحاضر</th>
+              <th rowSpan="2" className='col-present' style={{ padding: '0' }}>حاضر</th>
+              <th colSpan="4">4</th>
+              <th colSpan="4">3</th>
+              <th colSpan="4">2</th>
+              <th colSpan="4">1</th>
+              <th rowSpan="2" colSpan="2" className='std-father-name'>د پلار نوم</th>
+              <th rowSpan="2" colSpan="2">نوم</th>
+            </tr>
+            <tr>
+              <th colSpan="2" width="50px"></th>
+              <th colSpan="2" height="50"></th>
+              <th colSpan="2"></th>
+              <th colSpan="2"></th>
+              <th colSpan="2"></th>
+              <th colSpan="2"></th>
+              <th colSpan="2"></th>
+              <th colSpan="2"></th>
+            </tr>
+            {/* // ** data loop area start */}
+            <tr style={{ borderTop: 0 }}>
+              <td height="25px"></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td width="5px"></td>
+              <td width="5px"></td>
+              <td colSpan="2" >خالد</td>
+              <td colSpan="2" >محمود</td>
+              <td width="88px" >23323</td>
+              <td>۲</td>
+            </tr>
 
-              />
-            </Col>
-          </Row>
-        </CardBody>
-      </Card>
+            {/* // ** data loop area end */}
 
-    </div>
+            <tr style={{ borderTop: 0 }}>
+              <td height="25px"></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td colSpan="6" >د استاد لاسلیک</td>
+            </tr>
+            <tr style={{ borderTop: 0 }}>
+              <td colSpan="25" ><h4>د محترم استاد څخه هیله کوو چې حاضرې په جدې ډول روزمره کنټرول کړې ، امضا او تر ډکیدو وروسته یې پر ټاکلې وخت ادارې ته تسلیم کړې</h4>
+              </td>
+            </tr>
+          </Table>
+        </Col>
+        <Row style={{ float: 'right', marginRight: '10px' }}>
+        </Row>
+      </div>
+
+    </Fragment>
   )
 }
-
-export default ChancesList
+export default CardActions
---
+++ App/client/src/views/attendances/list/indexOld.js
@@ -0,0 +1,93 @@
+// ** Styles
+import '@styles/react/apps/app-users.scss'
+// ** React Imports
+import { Fragment, useState, useEffect } from 'react'
+
+
+// ** Third Party Components
+import Select from 'react-select'
+
+import { selectThemeColors } from '@utils'
+import { Card, CardHeader, CardTitle, CardBody, Input, Row, Col, Label, CustomInput, Button } from 'reactstrap'
+
+// ** Styles
+import '@styles/react/libs/react-select/_react-select.scss'
+
+const ChancesList = () => {
+
+  const [currentRole, setCurrentRole] = useState({ value: '', label: 'Select Role' })
+  const [currentPlan, setCurrentPlan] = useState({ value: '', label: 'Select Plan' })
+  const [currentStatus, setCurrentStatus] = useState({ value: '', label: 'Select Status', number: 0 })
+      // ** User filter options
+  const roleOptions = [
+    { value: '', label: 'Select Role' },
+    { value: 'admin', label: 'Admin' },
+    { value: 'author', label: 'Author' },
+    { value: 'editor', label: 'Editor' },
+    { value: 'maintainer', label: 'Maintainer' },
+    { value: 'subscriber', label: 'Subscriber' }
+  ]
+
+  const planOptions = [
+    { value: '', label: 'Select Plan' },
+    { value: 'basic', label: 'Basic' },
+    { value: 'company', label: 'Company' },
+    { value: 'enterprise', label: 'Enterprise' },
+    { value: 'team', label: 'Team' }
+  ]
+
+  const statusOptions = [
+    { value: '', label: 'Select Status', number: 0 },
+    { value: 'pending', label: 'Pending', number: 1 },
+    { value: 'active', label: 'Active', number: 2 },
+    { value: 'inactive', label: 'Inactive', number: 3 }
+  ]
+  return (
+    <div className='app-user-list'>
+            <Card>
+        <CardHeader>
+          <CardTitle tag='h4'>Search Filter</CardTitle>
+        </CardHeader>
+        <CardBody>
+          <Row>
+            <Col md='4'>
+              <Select
+                isClearable={false}
+                theme={selectThemeColors}
+                className='react-select'
+                classNamePrefix='select'
+                options={roleOptions}
+                value={currentRole}
+              />
+            </Col>
+            <Col className='my-md-0 my-1' md='4'>
+              <Select
+                theme={selectThemeColors}
+                isClearable={false}
+                className='react-select'
+                classNamePrefix='select'
+                options={planOptions}
+                value={currentPlan}
+
+              />
+            </Col>
+            <Col md='4'>
+              <Select
+                theme={selectThemeColors}
+                isClearable={false}
+                className='react-select'
+                classNamePrefix='select'
+                options={statusOptions}
+                value={currentStatus}
+
+              />
+            </Col>
+          </Row>
+        </CardBody>
+      </Card>
+
+    </div>
+  )
+}
+
+export default ChancesList
