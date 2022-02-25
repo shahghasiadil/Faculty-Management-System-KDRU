98af84505efb92d83d5232bc3dc3023c329a8643
zabihullah hamdard <zabih036@gmail.com>
2022-02-25 13:01:27 +0430

    Attendance Data Fileration Done

---
+++ App/client/src/views/attendances/store/action/actionTypes.js
@@ -0,0 +1,5 @@
+// ** Constants for Action types
+export const ARCHIVE = 'ARCHIVE'
+export const GET_ALL_DATA = 'GET_ALL_DATA'
+export const GET_DATA = 'GET_DATA'
+export const GET_STUDENTS = 'GET_STUDENTS'
