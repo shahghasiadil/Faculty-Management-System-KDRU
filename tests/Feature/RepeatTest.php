bb82ca5669d1dd13779a6f3c32a123b3f832614c
shahghasiadil <shaghasy555@gmail.com>
2022-03-05 08:58:27 +0430

    referesh database

--- tests/Feature/RepeatTest.php
+++ tests/Feature/RepeatTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class StudentTest extends TestCase
+class RepeatTest extends TestCase
 {
     use RefreshDatabase;
     /**
@@ -14,11 +14,9 @@ class StudentTest extends TestCase
      *
      * @return void
      */
-    public function test_student_can_be_returned()
+    public function test_repeat_get()
     {
-        $this->withoutExceptionHandling();
-        $response = $this->get('/api/students');
-        $response->assertOk();
+        $response = $this->get('/api/repeats');
+        $response->assertStatus(200);
     }
-
 }
