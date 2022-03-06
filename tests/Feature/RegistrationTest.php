4a33df053ddf8d5c9237cb77fcb658e7dfc3dc72
Ehsan-sudo <popalehsan26@gmail.com>
2022-03-06 10:11:23 +0430

    registration test updated

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -14,13 +14,6 @@ class RegistrationTest extends TestCase
      *
      * @return void
      */
-    public function test_registration_can_be_get()
-    {
-        $this->withoutExceptionHandling();
-        $response = $this->get('/api/registrations');
-        $response->assertStatus(200);
-    }
-
     public function test_registration_can_be_added()
     {
         $response = $this->post('/api/registrations',['subject_id' => 1 , 'student_id' => 1]);
